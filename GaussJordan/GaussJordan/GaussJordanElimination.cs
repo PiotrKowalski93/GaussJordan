a9263afa0174e7a825e8b485b8543f9e8e849ef9
Piotr Kowalski <kowalskii1@op.pl>
2017-01-09 21:08:05 +0100

    Gauss Jordan Elimination Method

---
+++ GaussJordan/GaussJordan/GaussJordanElimination.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussJordan
+{
+    public class GaussJordanElimination
+    {
+        public double[] Calcuale(double[,] A, double[] b, int n)
+        {
+            double[] x = new double[n];
+            double[,] tmpA = new double[n, n + 1];
+            double tmp = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    tmpA[i, j] = A[i, j];
+                }
+                tmpA[i, n] = b[i];
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                tmp = tmpA[k, k];
+                for (int i = 0; i < n + 1; i++)
+                {
+                    tmpA[k, i] = tmpA[k, i] / tmp;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (i != k)
+                    {
+                        tmp = tmpA[i, k] / tmpA[k, k];
+                        for (int j = k; j < n + 1; j++)
+                        {
+                            tmpA[i, j] -= tmp * tmpA[k, j];
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = tmpA[i, n];
+            }
+
+            return x;
+        }
+    }
+}
---
+++ GaussJordan/GaussJordan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussJordan
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            double[] result;
+            double[,] A = new double[3, 3] { { 1, -2, 3 },
+                                         { 2, 1, 1},
+                                         { -3, 2, -2 } };
+            double[] b = { 7, 4, -10 };
+
+            GaussJordanElimination obj = new GaussJordanElimination();
+
+            result = obj.Calcuale(A, b, b.Count());
+
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Console.WriteLine(result[i] + " ");
+            }
+
+            Console.ReadKey();
+        }        
+    }
+}
