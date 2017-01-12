0adf8d9c645010567036ee88863401f4f04a1fb0
Piotr Kowalski <kowalskii1@op.pl>
2017-01-12 20:25:52 +0100

    parallel

--- GaussJordan/GaussJordan/GaussJordanElimination.cs
+++ GaussJordan/GaussJordan/GaussJordanElimination.cs
@@ -8,35 +8,35 @@ namespace GaussJordan
 {
     public class GaussJordanElimination
     {
-        public double[] Calcuale(double[,] A, double[] b, int n)
+        public double[] Calcuale(double[,] A, double[] I, int rowsNumber)
         {
-            double[] x = new double[n];
-            double[,] tmpA = new double[n, n + 1];
+            double[] x = new double[rowsNumber];
+            double[,] tmpA = new double[rowsNumber, rowsNumber + 1];
             double tmp = 0;
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < rowsNumber; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < rowsNumber; j++)
                 {
                     tmpA[i, j] = A[i, j];
                 }
-                tmpA[i, n] = b[i];
+                tmpA[i, rowsNumber] = I[i];
             }
 
-            for (int k = 0; k < n; k++)
+            for (int k = 0; k < rowsNumber; k++)
             {
                 tmp = tmpA[k, k];
-                for (int i = 0; i < n + 1; i++)
+                for (int i = 0; i < rowsNumber + 1; i++)
                 {
                     tmpA[k, i] = tmpA[k, i] / tmp;
                 }
 
-                for (int i = 0; i < n; i++)
+                for (int i = 0; i < rowsNumber; i++)
                 {
                     if (i != k)
                     {
                         tmp = tmpA[i, k] / tmpA[k, k];
-                        for (int j = k; j < n + 1; j++)
+                        for (int j = k; j < rowsNumber + 1; j++)
                         {
                             tmpA[i, j] -= tmp * tmpA[k, j];
                         }
@@ -44,12 +44,77 @@ namespace GaussJordan
                 }
             }
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < rowsNumber; i++)
             {
-                x[i] = tmpA[i, n];
+                x[i] = tmpA[i, rowsNumber];
             }
 
             return x;
         }
+        
+        public double[] CalcualeParallel(double[,] MatrixA, double[] MatrixI, int rowsNumber)
+        {
+            List<Task> tasks = new List<Task>();
+
+            double[] result = new double[rowsNumber];
+            double[,] tempMatrix = new double[rowsNumber, rowsNumber + 1];
+            double diagonalValue;
+
+            // Prepare Temporaty Matrix
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                for (int j = 0; j < rowsNumber; j++)
+                {
+                    tempMatrix[i, j] = MatrixA[i, j];
+                }
+                tempMatrix[i, rowsNumber] = MatrixI[i];
+            }
+
+            // Do Calculations
+            for (int k = 0; k < rowsNumber; k++)
+            {
+                diagonalValue = tempMatrix[k, k];
+
+                for (int i = 0; i < rowsNumber + 1; i++)
+                {
+                    int tmp = i;
+
+                    tasks.Add(Task.Factory.StartNew(() =>
+                    {
+                        tempMatrix[k, tmp] = tempMatrix[k, tmp] / diagonalValue;
+                    }));
+                }
+                Task.WaitAll(tasks.ToArray());
+                tasks.Clear();
+
+                for (int i = 0; i < rowsNumber; i++)
+                {
+                    if (i != k)
+                    {
+                        int tmp = i;
+
+                        tasks.Add(Task.Factory.StartNew(() =>
+                        {
+                            diagonalValue = tempMatrix[tmp, k] / tempMatrix[k, k];
+                            for (int j = k; j < rowsNumber + 1; j++)
+                            {
+                                tempMatrix[tmp, j] -= diagonalValue * tempMatrix[k, j];
+                            }
+                        }));                        
+                    }
+                }
+                Task.WaitAll(tasks.ToArray());
+                tasks.Clear();
+            }
+
+            // Prepare and return result table
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                result[i] = tempMatrix[i, rowsNumber];
+            }
+
+            return result;
+        }
+        
     }
 }
--- GaussJordan/GaussJordan/Program.cs
+++ GaussJordan/GaussJordan/Program.cs
@@ -14,18 +14,27 @@ namespace GaussJordan
             double[,] A = new double[3, 3] { { 1, -2, 3 },
                                          { 2, 1, 1},
                                          { -3, 2, -2 } };
-            double[] b = { 7, 4, -10 };
+            double[] I = { 7, 4, -10 };
 
             GaussJordanElimination obj = new GaussJordanElimination();
 
-            result = obj.Calcuale(A, b, b.Count());
+            result = obj.Calcuale(A, I, I.Count());
 
+            PrintResultTable(result);
+
+            result = obj.CalcualeParallel(A, I, I.Count());
+
+            PrintResultTable(result);
+
+            Console.ReadKey();
+        }        
+
+        static void PrintResultTable(double[] result)
+        {
             for (int i = 0; i < result.Count(); i++)
             {
                 Console.WriteLine(result[i] + " ");
             }
-
-            Console.ReadKey();
-        }        
+        }
     }
 }
