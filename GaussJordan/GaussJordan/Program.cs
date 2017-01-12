b83b95b1614a94ca53d61eec5223234732c73822
marr <mgadzalski@gmail.com>
2017-01-12 21:07:47 +0100

    generowanie tablic (robocze)

--- GaussJordan/GaussJordan/Program.cs
+++ GaussJordan/GaussJordan/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,24 +8,38 @@ using System.Threading.Tasks;
 namespace GaussJordan
 {
     class Program
-    {      
+    {
+        static double[,] a;
+        static double[] b;
+        static int n = 1024;
+
+
         static void Main(string[] args)
         {
+            Stopwatch watch = new Stopwatch();
+            PrepareData();
             double[] result;
-            double[,] A = new double[3, 3] { { 1, -2, 3 },
-                                         { 2, 1, 1},
-                                         { -3, 2, -2 } };
-            double[] I = { 7, 4, -10 };
+            //double[,] A = new double[4,4] { { 1, -2, 3, 3 },
+            //                             { 2, 1, 1, 7},
+            //                             { -3, 2, -2, 1 },
+            //                            { -1, 3, -1, 2 }};
+            //double[] I = { 4, 1, -2,0 };
 
             GaussJordanElimination obj = new GaussJordanElimination();
 
-            result = obj.Calcuale(A, I, I.Count());
+            watch.Start();
+            result = obj.Calcuale(a, b, n);
+            watch.Stop();
+            //PrintResultTable(result);
+            Console.WriteLine(watch.ElapsedMilliseconds);
 
-            PrintResultTable(result);
-
-            result = obj.CalcualeParallel(A, I, I.Count());
-
-            PrintResultTable(result);
+            watch.Reset();
+            watch.Start();
+            result = obj.CalcualeParallel(a, b, n);
+            watch.Stop();
+            Console.WriteLine("");
+            //PrintResultTable(result);
+            Console.WriteLine(watch.ElapsedMilliseconds);
 
             Console.ReadKey();
         }        
@@ -34,6 +49,23 @@ namespace GaussJordan
             for (int i = 0; i < result.Count(); i++)
             {
                 Console.WriteLine(result[i] + " ");
+            }
+        }
+
+        static void PrepareData()
+        {
+            a = new double[n, n];
+            b = new double[n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < n; j++)
+                {
+                    a[i,j] = rand.NextDouble();
+                }
+                b[i] = rand.NextDouble();
+
             }
         }
     }
